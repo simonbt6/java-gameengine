de0edad0ec22f31b7ecc9e658f18036d15dfae04
Simon Brisebois-Therrien <simonbt6@gmail.com>
2021-09-17 22:08:10 -0400

    Init app

---
+++ src/main/java/simonbt6/App.java
@@ -0,0 +1,16 @@
+package simonbt6;
+
+import org.lwjgl.*;*
+import org.lwjgl.glfw.*;
+
+/*
+ * @author Simon Brisebois-Therrien
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
